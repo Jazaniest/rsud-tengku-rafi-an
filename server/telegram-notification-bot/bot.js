dbb9ad399c124f972ed358c0a5474a33488b8f4c
Jazaniest <Jazaniezt07@gmail.com>
2025-06-11 21:56:03 +0700

    mengupdate bot telegram agar bisa menghapus akun melalui bot

--- server/telegram-notification-bot/bot.js
+++ server/telegram-notification-bot/bot.js
@@ -19,10 +19,10 @@ bot.on('message', (msg) => {
   console.log('  text       :', text);
 });
 
-async function askConnectTelegram(chatId, username, usersTel) {
+async function askConnectTelegram(chatId, username, usersTele) {
   await bot.sendMessage(
     chatId,
-    `Akun ini telah terdaftar dengan username ${usersTel}, apakah Anda tetap ingin menambahkan? (setuju/tidak)`
+    `Akun ini telah terdaftar dengan username ${usersTele}. apakah Anda tetap ingin menambahkan? (setuju/tidak)`
   );
 
   bot.once('message', async (msg) => {
@@ -106,6 +106,101 @@ async function askMoveUsername(chatId, username) {
   });
 }
 
+async function askDeleteUser(chatId, user) {
+  await bot.sendMessage(chatId, `Ingin melepaskan tautan ke akun ${user} di website?(setuju/tolak)`)
+  bot.once('message', async (msg) => {
+    if(msg.chat.id != chatId) return;
+
+    const jawaban = msg.text.trim().toLowerCase();
+
+    if(jawaban == 'setuju') {
+      await pool.query(
+        'DELETE FROM user_telegram WHERE telegram_id = ?',
+        [chatId]
+      )
+      await bot.sendMessage(chatId, 'Berhasil melepaskan tautan akun!')
+    }
+    else if(jawaban == 'tolak') {
+      await bot.sendMessage(chatId, 'Baik, proses dibatalkan!')
+    } else {
+      await bot.sendMessage(chatId, 'Maaf, saya tidak mengerti. tolong jawab dengan "setuju" atau "tidak".')
+      askDeleteUser(chatId, user);
+    }
+  })
+}
+
+async function askDeleteUserAll(chatId, user) {
+  await bot.sendMessage(chatId, 'anda memiliki beberapa akun yang tertaut. hapus semua atau beberapa?(semua/beberapa)')
+
+  bot.once('message', async (msg) => {
+    if(msg.chat.id != chatId) return;
+
+    const jawaban = msg.text.trim().toLowerCase();
+
+    if(jawaban == 'semua') {
+      askDeleteUserAllAll(chatId);
+    }
+    else if(jawaban == 'beberapa') {
+      await bot.sendMessage(chatId, 'Ketikkan 1 akun yang ingin anda hapus!')
+      await bot.sendMessage(chatId, `berikut list user ${user}`);
+      inputUserDelete(chatId);
+    } else {
+      await bot.sendMessage(chatId, 'Maaf, saya tidak mengerti. tolong jawab dengan "semua" atau "beberapa"!')
+      askDeleteUserAll(chatId, user);
+    }
+
+  })
+}
+
+async function askDeleteUserAllAll (chatId) {
+  await bot.sendMessage(chatId, 'Yakin ingin menghapus semua akun tertaut?(yakin/tidak)')
+
+  bot.once('message', async (msg) => {
+    if(msg.chat.id != chatId) return;
+
+    const jawaban = msg.text.trim().toLocaleLowerCase();
+
+    if(jawaban == 'yakin') {
+      await pool.query(
+        'DELETE FROM user_telegram WHERE telegram_id = ?',
+        [chatId]
+      )
+      await bot.sendMessage(chatId, 'Berhasil menghapus semua akun yang tertaut!')
+    }
+    else if (jawaban == 'tidak') {
+      await bot.sendMessage(chatId, 'Proses dibatalkan!')
+    } else {
+      await bot.sendMessage(chatId, 'Maaf, saya tidak mengerti. tolong jawab dengan "yakin" atau "tidak"!')
+      askDeleteUserAllAll(chatId);
+    }
+  })
+}
+
+async function inputUserDelete(chatId) {
+  bot.once('message', async (msg) => {
+
+  const jawaban = msg.text.trim();
+  const hasil = await pool.query(
+    'SELECT * FROM user_telegram WHERE user_id = ?',
+    [jawaban]
+  )
+  const hasilAkhir = hasil[0];
+  console.log('isi hasil : ', hasilAkhir.length);
+
+  if(hasilAkhir == 0) {
+    await bot.sendMessage(chatId, 'Maaf, username yang anda masukkan tidak sesuai. silahkan masukkan kembali!')
+    inputUserDelete(chatId);
+  } else {
+    await pool.query(
+      'DELETE FROM user_telegram WHERE user_id = ?',
+      [jawaban]
+    )
+    await bot.sendMessage(chatId, `Berhasil menghapus tautan dengan user ${jawaban}`);
+  }
+
+})
+}
+
 
 bot.onText(/\/start(?:\s+(.+))?/, async (msg) => {
   const chatId = msg.chat.id;
@@ -158,19 +253,21 @@ bot.onText(/\/register(?:\s+(.+))?/, async (msg, match) => {
     const users = user[0]?.[0]?.username || null;
     const usersTel = userTel[0]?.[0]?.user_id || null;
     const usersId = userId[0]?.[0]?.user_id || null;
+    const userTele = userTel[0];
+    const usersTele = userTele.map(userTel => userTel.user_id);
 
     if (users == null) {
       await bot.sendMessage(chatId, '❌ Tidak ada username yang terdaftar di website dengan username tersebut!');
     }
-    else if (usersTel != null && usersId != null) {
-      await bot.sendMessage(chatId, 'Tidak perlu mendaftar kembali, akun anda sudah terdaftar 😊')
-    }
     else if (usersTel != null) {
-      askConnectTelegram(chatId, username, usersTel);
+      askConnectTelegram(chatId, username, usersTele);
     }
     else if (usersId != null) {
       askMoveUsername(chatId, username);
-    } else {
+    } 
+    else if (usersTel != null && usersId != null) {
+      await bot.sendMessage(chatId, 'Tidak perlu mendaftar kembali, akun anda sudah terdaftar 😊')
+    }else {
         try {
         const now = new Date();
         const sql = `
@@ -199,13 +296,37 @@ bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
 
   if (text.startsWith('/')) {
-    const handledCommands = ['/start', '/register'];
+    const handledCommands = ['/start', '/register', '/detach'];
 
     const command = text.split(' ')[0];
 
     if (!handledCommands.includes(command)) {
-      await bot.sendMessage(chatId, '❌ Perintah tersebut tidak tersedia di bot ini! Gunakan perintah /start atau /register');
+      await bot.sendMessage(chatId, '❌ Perintah tersebut tidak tersedia di bot ini! Gunakan perintah /start, /register dan /detach');
     }
+  }
+});
+
+
+bot.onText(/\/detach(?:\s+(.+))?/, async (msg) => {
+  const chatId = msg.chat.id;
+
+  try {
+    const row = await pool.query(
+      'SELECT * FROM user_telegram WHERE telegram_id = ?',
+      [chatId]
+    )
+
+    const rows = row[0];
+    const user = rows.map(row => row.user_id);
+
+    if (rows.length == 1) {
+      askDeleteUser(chatId, user);
+    }
+    else if(rows.length > 1) {
+      askDeleteUserAll(chatId, user);
+    }
+  } catch(error) {
+    await bot.sendMessage(chatId, 'Error saat melepaskan tautan akun!')
   }
 });
 
